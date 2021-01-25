b0a5eec377cdd6f87378e8fbc8a6cd9540057133
nassorh <nassorh.dev@gmail.com>
2021-01-25 20:14:42 +0000

    Day 24

--- Day 23 - Next Permutation.kts
+++ Day 23 - Next Permutation.kts
@@ -1,5 +1,27 @@
+import java.util.*
 
-//First find the first decreasing element from the right side
+//The next permutation is when you find the first decreasing element looping reverse you then swap this for the next
+//Largest element after the first decreasing element and swap them. Then you reverse all the elements after the
+//Index of the orginal first decreasing element (now the largest element after the first decreasing element is located)
 
+//Flag is false
+//Loop throught the array in reverse
+    //If a[i] < a[i-1] swap and flag is true
+//If the flag is false no next premutation break complete
 
-//
+//Find the next largest element after the first decreasing element
+//Swap this
+
+fun nextPermuatation(array: MutableList<Int>){
+    array.add(4)
+}
+
+fun main() {
+    var a = arrayListOf(1,2,3)
+    nextPermuatation(a)
+    println(a)
+    val test: Short = 10
+    println(test)
+}
+
+main()
