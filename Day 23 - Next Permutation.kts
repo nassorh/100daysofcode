98442f5111a06266f0cb3f31e0928133b88df71a
nassorh <nassorh.dev@gmail.com>
2021-01-26 11:09:37 +0000

    Day 25

--- Day 23 - Next Permutation.kts
+++ Day 23 - Next Permutation.kts
@@ -1,27 +1,52 @@
 import java.util.*
 
-//The next permutation is when you find the first decreasing element looping reverse you then swap this for the next
-//Largest element after the first decreasing element and swap them. Then you reverse all the elements after the
-//Index of the orginal first decreasing element (now the largest element after the first decreasing element is located)
 
-//Flag is false
-//Loop throught the array in reverse
-    //If a[i] < a[i-1] swap and flag is true
-//If the flag is false no next premutation break complete
-
-//Find the next largest element after the first decreasing element
-//Swap this
-
+fun swap(array:MutableList<Int>, firstIndex: Int,secondIndex: Int){
+    val temp = array[firstIndex]
+    array[firstIndex] = array[secondIndex]
+    array[secondIndex] = temp
+}
 fun nextPermuatation(array: MutableList<Int>){
-    array.add(4)
+    /*
+    //The next permutation is when you find the first decreasing element looping reverse next you find the largest
+    element after the first decreasing element and swap them. Then you reverse all the elements after the index of
+    the orginal first decreasing element (now the largest element after the first decreasing element is located)
+    */
+
+    //Flag is false
+    var Flag = false
+
+    //Loop throught the array in reverse
+    for (i in array.size -1 downTo 1){
+        //If a[i] < a[i-1]
+        if (array.get(i-1) < array.get(i)){
+            var ReverseIndex = i-1
+            break
+        }
+    }
+    //If the flag is false no next premutation break complete
+    if (Flag == false){
+        return
+    }
+
+    //Find the next largest element after the first decreasing element
+    var max = ReverseIndex
+    val min = ReverseIndex
+    var nextLargestElemenet
+    for (i in 0..array.size-1){
+        if (array.get(i) > array.get(min) && array.get(i) < array.get(max)){
+            nextLargestElemenet
+        }
+    }
+
+
+    //Swap this
 }
 
 fun main() {
-    var a = arrayListOf(1,2,3)
+    var a = arrayListOf(1,3,4,5,2) //Output 1,3,5,2,4
     nextPermuatation(a)
     println(a)
-    val test: Short = 10
-    println(test)
 }
 
 main()
